4ed37661f647041057327f75f1b654ebf6473b3c
klawrawkz <d_h_morrison@hotmail.com>
2021-02-16 16:31:34 -0500

    repair misspelled "scipt" to "script"

--- Hands-on lab/Resources/PostRebootConfigure.ps1
+++ Hands-on lab/Resources/PostRebootConfigure.ps1
@@ -202,7 +202,7 @@ $azcopy = "$opsDir\azcopy_windows_amd64_10.1.1\azcopy.exe"
 Write-Output "Download nested VM zip files using AzCopy"
 #$sourceFolder = 'https://cloudworkshop.blob.core.windows.net/line-of-business-application-migration/sept-2020'
 #$sourceFolder = 'https://rbsdemomgr8projstore-microsoftrouting.file.core.windows.net/migrate-resources/?sv=2019-12-12&ss=f&srt=co&sp=rwlc&se=2021-02-16T08:25:12Z&st=2021-02-16T00:25:12Z&spr=https&sig=052jLLC2ckxdBAtSE8%2B9nlUS1ozFSnV3nHI18djDJfo%3D'
-$sourceFolder = 'https://rbsdemomgr8projstore.file.core.windows.net/rbsmusicassets/migrate-resources?sv=2019-12-12&ss=f&srt=co&sp=rwdlc&se=2021-02-17T01:53:35Z&st=2021-02-16T17:53:35Z&spr=https&sig=KaCCNzGJiQYypsLTL6CfUkmHfqHfHqAvWCEDJVWoUuk%3D'
+$sourceFolder = 'https://rbsdemomgr8projstore.file.core.windows.net/rbsmusicassets/migrate-resources??sv=2020-02-10&ss=f&srt=co&sp=rwdlc&se=2026-02-18T05:00:00Z&st=2021-02-16T14:00:00Z&spr=https&sig=q83FMmbvbK01HOuazSj8ysvKF4hhV8Xa5UFqCfKrN7c%3D'
 
 
 # https://cloudworkshop.blob.core.windows.net/line-of-business-application-migration/sept-2020/SmartHotelWeb1.zip
